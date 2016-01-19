df0e84ee6b4923f6971c7dec853729b2595080d9
Jatstar <291206326@qq.com>
2016-01-20 00:01:00 +0800

    添加加号按钮

--- JYWeibo-Swift/Classes/Main/MainViewController.swift
+++ JYWeibo-Swift/Classes/Main/MainViewController.swift
@@ -14,11 +14,46 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
         tabBar.tintColor = UIColor.orangeColor()
         
+        addChildViewControllers()
+       
+    }
+    /**
+     添加子控制器
+     */
+    private func addChildViewControllers(){
         addChildViewController(HomeViewController(), title: "首页", imageName: "tabbar_home")
         addChildViewController(MessageViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(UIViewController(), title: "", imageName: "")
         addChildViewController(DiscoverViewController(), title: "广场", imageName: "tabbar_discover")
         addChildViewController(ProfileViewController(), title: "我", imageName: "tabbar_profile")
+        
     }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        let width = UIScreen.mainScreen().bounds.size.width / CGFloat(viewControllers!.count)
+        let rect = CGRect(x: 0, y: 0, width: width, height: 49)
+        composeButton.frame = CGRectOffset(rect, width * 2, 0)
+        
+    }
+    
+    /**
+     按钮点击事件的调用是由 运行循环 监听并且以消息机制传递的，因此，按钮监听函数不能设置为 private
+     */
+    func composeBtnClick(){
+     print(__FUNCTION__)
+    }
+    lazy private var composeButton:UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: UIControlState.Normal)
+        button.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: UIControlState.Highlighted)
+        button.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: UIControlState.Normal)
+        button.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: UIControlState.Highlighted)
+        button.addTarget(self, action: "composeBtnClick", forControlEvents:.TouchUpInside)
+        //在闭包中必须写self
+        self.tabBar.addSubview(button)
+        return button
+    }()
     /**
      初始化子控制器
      
