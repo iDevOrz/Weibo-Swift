d5c4d8876c5aa8de8af0d6532cf9acc13c53a1e0
Jatstar <291206326@qq.com>
2016-01-19 21:55:34 +0800

    初始化项目
    设置Icon,Launch

---
+++ JYWeibo-Swift/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  JYWeibo-Swift
+//
+//  Created by 张建宇 on 16/1/19.
+//  Copyright © 2016年 张建宇. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = UIColor.whiteColor()
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
